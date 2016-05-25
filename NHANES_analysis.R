be6b2ef31ab26ca85a4da96a57591bdb0e70ab4d
Elliott Brannon <esbrannon@gmail.com>
2016-05-25 14:11:48 -0400

    Added NHANES_data.R to download the data. Worked on creating functions

--- NHANES_analysis.R
+++ NHANES_analysis.R
@@ -1,19 +1,21 @@
 # NHANES Mortality Data Analysis
-# Mortality Data - Lines 7 - 40
-# NHANES Data - Lines 44 - 70
+# Mortality Data
+# NHANES Survey/Lab Data
 
 library(nhanesdata)
 library(plyr)
 
-### Mortality Data
-
+## Global Variables
 years <- c("1999_2000", "2001_2002", "2003_2004", "2005_2006", "2007_2008", "2009_2010", "2011_2012", "2013_2014", "2015_2016")
 years2 <- gsub("_", "-", years)
-mort_filename1 = "ftp://ftp.cdc.gov/pub/Health_Statistics/NCHS/datalinkage/linked_mortality/NHANES_"
-mort_filename2 = "_MORT_2011_PUBLIC.dat"
 
-mort_widths = c(14,1,1,1,3,1,1,1,4,8,8,3,3,1,1,1,1,1)
-mort_names = 	c("publicid",       # str 14
+
+### Load NHANES mortality data for 1999-2016
+loadmortality <- function() { 
+  mort_filename1 = "ftp://ftp.cdc.gov/pub/Health_Statistics/NCHS/datalinkage/linked_mortality/NHANES_"
+  mort_filename2 = "_MORT_2011_PUBLIC.dat"
+  mort_widths = c(14,1,1,1,3,1,1,1,4,8,8,3,3,1,1,1,1,1)
+  mort_names = 	c("publicid",       # str 14
                 "eligstat", 		  # 15
                 "mortstat", 		  # 16
                 "causeavl",		    # 17
@@ -32,70 +34,76 @@ mort_names = 	c("publicid",       # str 14
                 "mortsrce_dc",		# 53
                 "mortsrce_dcl"    # 54 
                 )
-
-mort_data <- data.frame()
-for (item in years) {
-  mort_data <- rbind(mort_data, read.fwf(file=paste0(mort_filename1, item, mort_filename2), 
+  mort_data <- data.frame()
+  for (item in years) {
+    mort_data <- rbind(mort_data, read.fwf(file=paste0(mort_filename1, item, mort_filename2), 
                                          col.names=mort_names, widths=mort_widths))
+  }
+  }
+
+####################################
+
+
+### NHANES metadata
+metadata <- function (){
+  datasets <- c('demo', 'dietary', 'exam', 'lab', 'question', 'limited')
+  for (item in datasets) {
+    temp <- cbind(item, read.table(paste0("./", item, ".txt"), "\t", header=FALSE))
+    assign(item, temp)
+  }
+  datasets <- rbind(demo, dietary, exam, lab, question, limited)
+  colnames(datasets)[1] <- 'dataset'
+  datasets[[7]] <- gsub(" *.* ", "", datasets[[4]])
+  datasets[[4]] <- gsub(" .*", "", datasets[[4]])
+  rm(temp,demo,dietary,exam,lab,question,limited)
+  return(datasets)
 }
-
-### Mortality data
-
-
-### NHANES data
-
-datasets <- c('demo', 'dietary', 'exam', 'lab', 'question', 'limited')
-for (item in datasets) {
-  temp <- cbind(item, read.table(paste0("./", item, ".txt"), "\t", header=FALSE))
-  assign(item, temp)
-}
-datasets <- rbind(demo, dietary, exam, lab, question, limited)
-colnames(datasets)[1] <- 'dataset'
-datasets[[7]] <- gsub(" *.* ", "", datasets[[4]])
-datasets[[4]] <- gsub(" .*", "", datasets[[4]])
-rm(temp,demo,dietary,exam,lab,question,limited)
-
 
 
 
 ###################### Determine percentage of 
-datayear <- data.frame(cbind(as.character(datasets$V1), datasets$V3))
-colnames(datayear) <- c("years","set")
-datayear$type <- gsub("_.*$", "", datayear$set)
-datayear$from <- as.numeric(substr(datayear$years, 1, 4))
-datayear$to <- as.numeric(substr(datayear$years, 6, 9))
+dropbyyear <- function(p) {
+  datayear <- data.frame(cbind(as.character(datasets$V1), datasets$V3))
+  colnames(datayear) <- c("years","set")
+  datayear$set_base <- gsub("_.*$", "", datayear$set)
+  datayear$from <- as.numeric(substr(datayear$years, 1, 4))
+  datayear$to <- as.numeric(substr(datayear$years, 6, 9))
 
-datayear <- unique(adply(datayear, 1, summarise, year = seq(from, to))[c("set","type", "year")])
+  datayear <- unique(adply(datayear, 1, summarise, year = seq(from, to))[c("set","set_base", "year")])
 
+  for(y in unique(datayear$year)) {
+    datayear[paste("y",y,sep="")] <- ifelse(datayear$year==y,1,0) }
+  
+  yearsum <- aggregate(datayear[,4:19], by=list(Category=datayear$set_base), FUN=sum)
+  yearsum$avg <- rowMeans(yearsum[,2:17])
 
-for(y in unique(datayear$year)) {
-     datayear[paste("y",y,sep="")] <- ifelse(datayear$year==y,1,0) }
-
-yearsum <- aggregate(datayear[,4:19], by=list(Category=datayear$type), FUN=sum)
-yearsum$avg <- rowMeans(yearsum[,2:17])
-
-finalsets <- yearsum[which(yearsum$avg >= .75),1]
-
+  return(yearsum[which(yearsum$avg >= p),1])
+  }
 ####################
 
-datasets <- datasets[gsub("_.*$", "", datayear$set) %in% finalsets, ]
-
-
-
-
-
-data(list = tolower(datasets[[4]]))
-
-for (item in ls()) {
-  if(!("SEQN" %in% names(eval(parse(text = item))))) {
-    rm(list = item)  
+dropbyage <- function(age) {
+  demos <- tolower(datasets[which(gsub("_.*$", "", datasets$V3) == "DEMO" ),4])
+  demos <- demos[demos != "demo_h"]
+  data(list = demos)
+  demos <- rbind.fill(demo, demo_b, demo_c, demo_d, demo_e, demo_f, demo_g)
+  return(demos[which(demos$RIDAGEYR >= age), ])
   }
-}
 
-for (item in ls()) {
+
+
+datasets <- metadata()
+datasets <- datasets[which(datasets$V4 != 'RDC Only'),]
+finalsets <- dropbyyear(.5)
+finalsets <- finalsets[finalsets != "DEMO"]
+demos <- dropbyage(18)
+rm(list = c("demo", "demo_b", "demo_c", "demo_d", "demo_e", "demo_f", "demo_g"))
+datasets <- datasets[gsub("_.*$", "", datasets$V3) %in% finalsets, ]
+
+data(list = tolower(c("DEMOS", datasets[[4]])))
+
+
+for(item in unique(gsub("_.*$", "", datasets$V3))) {
   print(item)
-  if (item != "demo" && item != "zdemo" && item != "demo_b" && item != "demo_c") {
-    demo <- merge(demo, eval(parse(text = item)), by = "SEQN", all.x = TRUE)
-    demo <- Filter(function(x)!all(is.na(x)), demo)
-  }
+  tempdata <- do.call(rbind.fill, lapply(ls(pattern = tolower(item)), get))
+  merge(demos, tempdata, by = "SEQN", all.x = TRUE)
 }
---
+++ NHANES_data.R
@@ -0,0 +1,51 @@
+
+
+#download all datasets
+
+# getstr() is my customized function
+# http://www.r-bloggers.com/how-to-extract-a-string-between-2-characters-in-r-and-sas/
+# it extracts a string between 2 characters in a string variable
+getstr = function(mystring, initial.character, final.character)
+{
+  
+  # check that all 3 inputs are character variables
+  if (!is.character(mystring))
+  {
+    stop('The parent string must be a character variable.')
+  }
+  
+  if (!is.character(initial.character))
+  {
+    stop('The initial character must be a character variable.')
+  }
+  
+  
+  if (!is.character(final.character))
+  {
+    stop('The final character must be a character variable.')
+  }
+  
+  # pre-allocate a vector to store the extracted strings
+  snippet = rep(0, length(mystring))
+  
+  for (i in 1:length(mystring))
+  {
+    # extract the initial position
+    initial.position = gregexpr(initial.character, mystring[i])[[1]][1] + 1
+    
+    # extract the final position
+    final.position = gregexpr(final.character, mystring[i])[[1]][1] - 1
+    
+    # extract the substring between the initial and final positions, inclusively
+    snippet[i] = substr(mystring[i], initial.position, final.position)
+  }
+  
+  return(snippet)
+}
+
+
+thepage = readLines('http://wwwn.cdc.gov/Nchs/Nhanes/Search/DataPage.aspx?Component=Demographics')
+mypattern = '/Nchs/Nhanes/.*.XPT'
+datalines = grep(mypattern,thepage,value=TRUE)
+getstr(datalines,"/Nchs",".XPT")
+
